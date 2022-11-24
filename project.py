79e1c1dcd59283d9d5514e87db6824aa2a5491e7
“[firstname <pawan06kumar2003@gmail.com>
2022-11-24 21:57:32 +0530

    Pawan made the battery remainder code

--- project.py
+++ project.py
@@ -1,1 +1,22 @@
-print("our project")
+import time
+import psutil
+from plyer import notification
+
+print("Started....")
+
+cBatteryPercent = 0
+pBatteryPercent = 0
+
+if psutil.sensors_battery().power_plugged: #If charging is active
+    pBatteryPercent = 0
+    while True:
+        cBattery = psutil.sensors_battery()
+        CurrentBatteryPercent = cBattery.percent
+        cBatteryPercent = CurrentBatteryPercent
+        if cBatteryPercent >= pBatteryPercent:
+            title = "Battery Percent Reminder" #display the notification title
+            message = f'Your current battery percent is {CurrentBatteryPercent}'
+            notification.notify(title = title, message = message)
+            pBatteryPercent = cBatteryPercent + 1
+        time.sleep(1)
+
